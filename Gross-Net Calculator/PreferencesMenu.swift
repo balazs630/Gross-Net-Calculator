6e0f609ee83f6de784c0e98c4230036ec22c7a01
Horváth Balázs <balazs630@icloud.com>
2016-11-26 23:34:31 +0100

    Add preferences window initial version

---
+++ Gross-Net Calculator/PreferencesMenu.swift
@@ -0,0 +1,18 @@
+//
+//  PreferencesMenu.swift
+//  Gross-Net Calculator
+//
+//  Created by Horváth Balázs on 2016. 11. 26..
+//  Copyright © 2016. Horváth Balázs. All rights reserved.
+//
+
+
+import Cocoa
+
+class PreferencesMenu: NSViewController {
+    
+    @IBOutlet weak var vatRate: NSTextField!
+    @IBOutlet weak var currency: NSTextField!
+
+    
+}
