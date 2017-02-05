bf57292fd524c498d5a0f56d702bf4acf623880d
Horváth Balázs <balazs630@icloud.com>
2017-02-05 21:13:44 +0100

    Save vatRate and currency values to UserDefaults

--- Gross-Net Calculator/PreferencesMenu.swift
+++ Gross-Net Calculator/PreferencesMenu.swift
@@ -13,14 +13,55 @@ import Cocoa
 class PreferencesMenu: NSViewController {
     
     @IBOutlet weak var vatRate: NSTextField!
+    @IBOutlet weak var radioButtonFt: NSButton!
+    @IBOutlet weak var radioButtonEuro: NSButton!
+    @IBOutlet weak var radioButtonDollar: NSButton!
     
+    var prefs: UserDefaults = UserDefaults.standard
+    
+    @IBAction func chooseCurrency(_ sender: NSButton) {
+        prefs.set(sender.title, forKey: "currency")
+        prefs.synchronize()
+    }
     
     @IBAction func doneButton(_ sender: Any) {
+        let enteredVatValue = Int(vatRate.stringValue)
+        if enteredVatValue == nil {
+            
+        } else {
+            prefs.set(vatRate.stringValue, forKey: "vatRate")
+        }
+        
+        prefs.synchronize()
         self.view.window?.close()
     }
     
+    func checkUserDefaultsExist() {
+        if prefs.object(forKey: "vatRate") == nil {
+            prefs.set(27, forKey: "vatRate")
+        }
+        if prefs.object(forKey: "currency") == nil {
+            radioButtonFt.state = 1
+        }
+        prefs.synchronize()
+    }
+    
     override func viewDidAppear() {
-        vatRate.stringValue = "27"
+        checkUserDefaultsExist()
+        
+        let radioButtonState = prefs.string(forKey: "currency")!
+        switch radioButtonState {
+        case "Ft":
+            radioButtonFt.state = 1
+        case "€":
+            radioButtonEuro.state = 1
+        case "$":
+            radioButtonDollar.state = 1
+        default:
+            NSLog("Unexpected currency was selected")
+        }
+
+        vatRate.stringValue = prefs.string(forKey: "vatRate")!
     }
 
 }
